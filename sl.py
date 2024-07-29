38f69d3ea185361f056b8ab7fcb26a274a1991df
tuan212003 <tuan95212003@gmail.com>
2024-07-29 15:14:50 +0700

    add file

--- sl.py
+++ sl.py
@@ -18,17 +18,17 @@ if video_file is not None:
     file_name = video_file.name
 VideoFolder='/content/Vid23d/ResultFolder/'
 VideoPath = VideoFolder + file_name  
-tracking_method = st.radio("Choose an option:", ["bbox", "pose"],index=0,horizontal=True)
+# tracking_method = st.radio("Choose an option:", ["bbox", "pose"],index=0,horizontal=True)
 detector = st.radio("Choose an option:", ["yolo", "maskrcnn"],index=0,horizontal=True)
-if tracking_method=='pose':
-    run_smplify = st.checkbox("Chạy SMPLify", value=True)
+# if tracking_method=='pose':
+#     run_smplify = st.checkbox("Chạy SMPLify", value=True)
 sideview = st.checkbox("Góc nhìn thay thế", value=False)
 smooth = st.checkbox("Làm mượt", value=False)
 
 class Args:
     vid_file = ''
     output_folder = ''
-    tracking_method = '' 
+    tracking_method = 'box' 
     detector = '' 
     yolo_img_size = 416
     tracker_batch_size = 12
@@ -47,16 +47,17 @@ class Args:
 args=Args()
 args.vid_file=VideoPath
 args.output_folder=VideoFolder
-args.tracking_method=tracking_method
+# args.tracking_method=tracking_method
 args.detector=detector
-args.run_smplify=run_smplify
+# args.run_smplify=run_smplify
 args.sideview=sideview
 args.smooth=smooth
 
 st.button('Creat video')
 if st.button("Click me"):
     main(args)
+    name = os.path.splitext(file_name)[0]
+    video_output=VideoFolder+name+'/'+ name + '_vibe_result.mp4'
+    st.video(video_output)
 
-name = os.path.splitext(file_name)[0]
-video_output=VideoFolder+name+'/'+ name + '_vibe_result.mp4'
-st.video(video_output)
+
